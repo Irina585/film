9e9d4b51232a0970506f3ea38777a00a414eeea0
Irina585 <irina.pro84@mail.ru>
2022-04-12 19:30:45 +0300

    lessons 1,2

---
+++ lib/images.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+abstract class Images {
+  Images._();
+  static const String prettyWoman = 'assets/prettyWoman.jpg';
+  static const String garfild = 'assets/garfild.jpg';
+  static const String homeAlone ='assets/home_alone.jpg';
+  static const String dontLookUp = 'assets/dont_look_up.jpg';
+  static const String forestGump = 'assets/forest_gump.jpg';
+
+
+
+
+
+
+}
